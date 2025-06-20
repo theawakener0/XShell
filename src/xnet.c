0f2dd21c88edfa31ea87c23eb896aa3ad423c57b
theawakener0 <wakeupwithme000@gmail.com>
2025-06-20 16:48:18 +0300

    Add sys/ioctl.h include for improved socket handling on POSIX systems

--- src/xnet.c
+++ src/xnet.c
@@ -22,6 +22,7 @@
     #include <sys/types.h>
     #include <sys/time.h>
     #include <net/if.h>
+    #include <sys/ioctl.h>
     #define SOCKET int
     #define INVALID_SOCKET -1
     #define SOCKET_ERROR -1
