890f3a782d5f1ab7c029b21395ece8441cd23094
theawakener0 <wakeupwithme000@gmail.com>
2025-07-07 09:29:42 -0400

    Fixing errors in xcodex

--- src/xcodex.c
+++ src/xcodex.c
@@ -1063,15 +1063,6 @@ writeerr:
 
 /* ============================= Terminal update ============================ */
 
-/* We define a very simple "append buffer" structure, that is an heap
- * allocated string where we can append to. This is useful in order to
- * write all the escape sequences in a buffer and flush them to the standard
- * output in a single call, to avoid flickering effects. */
-struct abuf {
-    char *b;
-    int len;
-};
-
 #define ABUF_INIT {NULL,0}
 
 void abAppend(struct abuf *ab, const char *s, int len) {
